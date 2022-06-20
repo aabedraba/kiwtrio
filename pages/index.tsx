ae1968b0f02f26899de219be24903e2ca77c7ede
Abdallah Abedraba <aabedraba@gmail.com>
2022-06-20 20:58:02 +0400

    feat: add index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,56 @@
 const Index = () => {
-  return <div>Hello world!</div>
-}
+  return (
+    <div className="flex flex-col w-screen bg-blue-100">
+      <div className="h-[30vh] w-screen bg-cover bg-[url('/main.jpeg')]"></div>
+      <div className="mx-auto w-4/5">
+        <ul className="flex justify-evenly">
+          <li>Fotos</li>
+          <li>Videos</li>
+          <li>Tienda</li>
+          <li>Jazz</li>
+          <li>More jazz</li>
+        </ul>
+      </div>
+      <div>
+        <h1 className="text-center text-5xl">Kiw Trio</h1>
+      </div>
+
+      <div className="flex overflow-auto snap-x gap-6 remove-scrollbar">
+        <div className="md:ml-[50%] snap-center">
+          <iframe
+            height="315"
+            width="560"
+            src="https://www.youtube.com/embed/sc88ygob9RQ"
+            title="YouTube video player"
+          ></iframe>
+        </div>
+        <div className="snap-center">
+          <iframe
+            height="315"
+            width="560"
+            src="https://www.youtube.com/embed/sc88ygob9RQ"
+            title="YouTube video player"
+          ></iframe>
+        </div>
+        <div className="snap-center">
+          <iframe
+            height="315"
+            width="560"
+            src="https://www.youtube.com/embed/sc88ygob9RQ"
+            title="YouTube video player"
+          ></iframe>
+        </div>
+        <div className="snap-center">
+          <iframe
+            height="315"
+            width="560"
+            src="https://www.youtube.com/embed/sc88ygob9RQ"
+            title="YouTube video player"
+          ></iframe>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Index;
