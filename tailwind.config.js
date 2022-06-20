e481a46e30c67fab1b33d209d16a3df8a76b9f42
Manuel Alférez <manuelalferezruiz@gmail.com>
2022-06-20 19:15:14 +0200

    feat: added palette of colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
@@ -5,6 +7,42 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      lavendergray: "#CECEDF",
+      darkbluegray: "#5B5F97",
+      maximumyellow: "#5B5F97",
+      babypowder: "#FFFFFB",
+      bittersweet: "#FF6B6C",
+      darkblue: "#2077F9",
+      lightred: "#FF5656",
+      inherit: colors.inherit,
+      current: colors.current,
+      transparent: colors.transparent,
+      black: colors.black,
+      white: colors.white,
+      slate: colors.slate,
+      gray: colors.gray,
+      zinc: colors.zinc,
+      neutral: colors.neutral,
+      stone: colors.stone,
+      red: colors.red,
+      orange: colors.orange,
+      amber: colors.amber,
+      yellow: colors.yellow,
+      lime: colors.lime,
+      green: colors.green,
+      emerald: colors.emerald,
+      teal: colors.teal,
+      cyan: colors.cyan,
+      sky: colors.sky,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      violet: colors.violet,
+      purple: colors.purple,
+      fuchsia: colors.fuchsia,
+      pink: colors.pink,
+      rose: colors.rose,
+    },
   },
   plugins: [],
-}
+};
